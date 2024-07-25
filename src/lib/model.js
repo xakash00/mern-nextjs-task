c790caca1a208ad756489296de5b7fa0cb42ac51
akashsingh000 <akash.s@krishaweb.com>
2024-07-25 15:10:49 +0530

    initial commit

---
+++ src/lib/model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const studentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: [true, "Email already exist"],
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("invalid email");
+            }
+        },
+    },
+    phone: {
+        type: Number,
+        required: true,
+        min: 10
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+});
+
+//creating a new collection
+const Student = mongoose.models.Student || mongoose.model("Student", studentSchema);
+
+module.exports = Student;
--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,58 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import { NextApiRequest, NextApiResponse } from 'next';
+import Student from 'components/lib/model';
+import dbConnect from '../../lib/dbConnect';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+export default async function handler(req, res) {
+  const { method } = req;
+
+  await dbConnect();
+
+  switch (method) {
+    case 'GET':
+      handleGET(req, res)
+      break;
+    case 'POST':
+      handlePOST(req, res)
+    default:
+      break;
+  }
 }
+
+
+const handleGET = async (req, res) => {
+  // const currentUser = await (req);
+  try {
+    const studentsData = await Student.find();
+    res.send(studentsData);
+  } catch (err) {
+    res.send(err);
+  }
+};
+
+const handlePOST = async (req, res) => {
+  const { image } = req.body
+  // try {
+  //   if (image) {
+  //     let response = await axios.get(image, { responseType: 'arraybuffer' });
+  //     let returnedB64 = Buffer.from(response.data).toString('base64');
+  //     const result = `data:image/png;base64,${returnedB64}`
+  //     res.status(200).json({
+  //       data: result,
+  //     });
+  //   }
+  // } catch (err) {
+  //   console.log(err)
+  //   res.status(200).json({
+  //     data: err,
+  //   });
+  // }
+
+  try {
+    const user = new Student(req.body);
+    const createUser = await user.save();
+    res.status(201).send(createUser);
+  } catch (error) {
+    console.log(error)
+    res.status(400).send(error);
+  }
+};
